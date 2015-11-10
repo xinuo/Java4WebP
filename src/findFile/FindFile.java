7fe6657da658e5e5cf8acba584abec76aba667d0
Xinuo <Administrator@WIN-IFHBRA8SENK>
2015-11-10 09:08:50 +0800

    解决不能读取指定文件的错误.

--- src/findFile/FindFile.java
+++ src/findFile/FindFile.java
@@ -5,28 +5,32 @@ import java.io.File;
 import jpg2webp.Jpg2webP;
 
 public class FindFile {
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		Jpg2webP j2w = new Jpg2webP();
-		findfile("C:\\Users\\Administrator\\Downloads");
+		findfile("C:\\Users\\Administrator\\Downloads\\Compressed\\IMG_0073.JPG");
 	}
-	
+
 	/**
+	 * 指定文件路径或者文件夹路径进行转换
 	 * 
 	 * @param filepath
 	 */
-	static void findfile(String filepath){
+	static void findfile(String filepath) {
 		Jpg2webP j2w = new Jpg2webP();
 		File file = new File(filepath);
-		File[] templist = file.listFiles();
-		 for (int i = 0; i < templist.length; i++) {
-		   if (templist[i].isFile()&&(templist[i].getName().endsWith("JPG")||templist[i].getName().endsWith("jpg")||templist[i].getName().endsWith("Jpg"))) {
+		if (file.isFile()&&file.getName().toLowerCase().endsWith("jpg")) {
+			j2w.jpg2webP(file);
+		} else if (file.isDirectory()) {
+			File[] templist = file.listFiles();
+			for (int i = 0; i < templist.length; i++) {
+				if (templist[i].isFile() && templist[i].getName().toLowerCase().endsWith("jpg")) {
+					j2w.jpg2webP(templist[i]);
+				}
+				if (templist[i].isDirectory()) {
+					findfile(templist[i].getAbsolutePath());
+				}
+			}
+		}
+	}
 
-			    j2w.jpg2webP(templist[i]);
-			   }
-			   if (templist[i].isDirectory()) {
-				   findfile(templist[i].getAbsolutePath());
-			   }
-	}
-	}
-	
 }
